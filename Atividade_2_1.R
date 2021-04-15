b0e9c6107cba084e246ff04b5ebfb5c6adc7a5d7
Vlademir Monteiro <vlademir.monteiro@hotmail.com>
2021-04-15 16:34:59 -0300

    Arquivo contendo exemplo de data frame

---
+++ Atividade_2_1.R
@@ -0,0 +1,8 @@
+
+candidatos <- c("Lula", "Ciro", "Bolsonaro", "Doria", "Amoedo")
+
+partidos <- c("PT", "PDT", "S/P", "PSDB", "NOVO")
+
+ideologia <- c("esquerda", "esquerda", "extrema direita", "centro direita", "centro direita")
+
+banco1 <- data.frame(candidatos, partidos, ideologia)
